2e215543b0421c6afb8f5cbdf8ba06161957851e
MartenMeijboom <MartenMeijboom@users.noreply.github.com>
2019-05-30 15:53:32 +0200

    springboot project setup

---
+++ src/main/java/nl/saxion/jm/zeldasite/ZeldasiteApplication.java
@@ -0,0 +1,13 @@
+package nl.saxion.jm.zeldasite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeldasiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeldasiteApplication.class, args);
+    }
+
+}
