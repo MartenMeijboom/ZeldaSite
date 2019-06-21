aedc445a28e6f9bcb9b950d888237e90b8f27118
immink <467357@student.saxion.nl>
2019-06-21 04:08:54 +0200

    Gedeelte van css.

--- src/main/java/nl/saxion/jm/zeldasite/ZeldasiteApplication.java
+++ src/main/java/nl/saxion/jm/zeldasite/ZeldasiteApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZeldaSiteApplication {
+public class ZeldasiteApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZeldaSiteApplication.class, args);
+        SpringApplication.run(ZeldasiteApplication.class, args);
     }
 
 }
